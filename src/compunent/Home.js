7e62f624b0601300fcd223cbbb9dabd8a44e629f
mdasif526976 <mdasif526976@gmail.com>
2022-09-29 17:44:38 +0600

    added

---
+++ src/compunent/Home.js
@@ -0,0 +1,17 @@
+import "./Home/Home.css"
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <div className="practise">
+                <h1>im from practise</h1>
+            </div>
+            <div className="practise-time">
+                <h1> im from practide-time</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
